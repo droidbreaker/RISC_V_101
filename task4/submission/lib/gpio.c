93f9315c3449e672dbc03dd978aa84e00dde99f3
droidbreaker <braker.noob.kernel@gmail.com>
2026-05-29 15:55:11 +0530

    GPIO API description added

--- task4/submission/lib/gpio.c
+++ task4/submission/lib/gpio.c
@@ -43,29 +43,42 @@ static void gpio_enable_clock(GPIO_Port port)             // Helper function to 
 }
 
 
-
+/*
+ * @brief : This function configuring GPIOx PINx as OUTPUT Mode. 
+ * By configuring MODE and CNF using "@CFGLR" register, and make the pin as output Pull-up mode. 
+ * and clear that pin before the writting on it by clearing "@BCR" register corresponding pin bit.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
 void gpio_config_output(GPIO_Port port,GPIO_PIN pin)       // Helper function to configure the specified GPIO pin as output
 {
-    uint32_t shift = pin * 4;               // Each pin has 4 bits in the configuration register (2 for MODE and 2 for CNF)
-    gpio(port)->gpio_cfglr &= ~(0xF << shift);    // Clear the 4 bits corresponding to the pin
+    uint32_t shift = pin * 4;                              // Each pin has 4 bits in the configuration register (2 for MODE and 2 for CNF)
+    gpio(port)->gpio_cfglr &= ~(0xF << shift);             // Clear the 4 bits corresponding to the pin
 
-    gpio(port)->gpio_cfglr |= (0x3 << shift);       // Set MODE to 11b (50MHz)
-    gpio(port)->gpio_cfglr |= (0x0 << (shift + 2)); // Set CNF to 00 (Push-pull)  
-    gpio(port)->gpio_bcr = (0 << pin);              // Clear the bit corresponding to the pin to 0, which sets the pin low.
+    gpio(port)->gpio_cfglr |= (0x3 << shift);              // Set MODE to 11b (50MHz)
+    gpio(port)->gpio_cfglr |= (0x0 << (shift + 2));        // Set CNF to 00 (Push-pull)  
+    gpio(port)->gpio_bcr = (0 << pin);                     // Clear the bit corresponding to the pin to 0, which sets the pin low.
 }
 
-
+/*
+ * @brief : this function configuring GPIOx PINx as INPUT mode. By configuring MODE and CNF using "@CFGLR" register, 
+ * and make the pin as input-pull up in "@OUTDR" register ,so that that pin acts as input mode.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
 void gpio_config_input(GPIO_Port port,GPIO_PIN pin)
 {
-    uint32_t shift = pin * 4;                 // Each pin has 4 bits in the configuration register (2 for MODE and 2 for CNF)
-    gpio(port)->gpio_cfglr &= ~(0xF << shift);      // Clear the 4 bits corresponding to pin
+    uint32_t shift = pin * 4;                              // Each pin has 4 bits in the configuration register (2 for MODE and 2 for CNF)
+    gpio(port)->gpio_cfglr &= ~(0xF << shift);             // Clear the 4 bits corresponding to pin
     
-    gpio(port)->gpio_cfglr |= (0x8 << shift);     // MODE = 00, CNF = 10  -> 0x8 1000b = 0x8 
-    gpio(port)->gpio_outdr |= (1 << pin);            // pin as input pull-up mode. 
+    gpio(port)->gpio_cfglr |= (0x8 << shift);              // MODE = 00, CNF = 10  -> 0x8 1000b = 0x8 
+    gpio(port)->gpio_outdr |= (1 << pin);                  // pin as input pull-up mode. 
 }
 
 
-static void gpio_reset(GPIO_Port port)            // Helper function to reset the specified GPIO port
+static void gpio_reset(GPIO_Port port)                     // Helper function to reset the specified GPIO port
 {
     switch(port)
     {
@@ -86,6 +99,14 @@ static void gpio_reset(GPIO_Port port)            // Helper function to reset th
     }
 }
 
+/*
+ * @brief : this function resets all the GPIO ports , so that its registers were fully cleaned before any application usage.
+ * by using "@RCC_APB2PRSTR_REG" for the corresponding bit for PORTx. by resetting it to clear position.
+ * @param : void there is no need for argument for this function as it resets all GPIO ports.
+ * @return : nothing to return.
+ *
+ */
+
 void gpio_reset_all(void)
 {
     gpio_reset(gpio_portA);
@@ -94,7 +115,8 @@ void gpio_reset_all(void)
 }
 
 /* 
- * @brief this function initializes GPIO Port and Pins for the requirement of input or output mode.
+ * @brief this function initializes GPIO Port and Pins for the requirement of input or output mode. 
+ * Enabling the clock for required PORTx. "@RCC_APB2PCENR_REG" to turn on corresponding bit for PORTx.
  * @param port : [port A, port C , port D] ; pin : [0-7] pin ; in_out_mode : [1 (output) , 0 (input)].
  * @return nothing to return.
  */
@@ -110,24 +132,52 @@ void gpio_init(GPIO_Port port,uint8_t pin,uint8_t in_out_mode)       // Main fun
     }
 }
 
-
-void gpio_set_pin(GPIO_Port port, uint8_t pin)    // Function to set the specified GPIO pin
+/*
+ * @brief : this function will set the GPIOx PINx to HIGH 
+ * by setting the "@BSHR" registers corresponding bit to '1'.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
+void gpio_set_pin(GPIO_Port port, uint8_t pin)                     // Function to set the specified GPIO pin
 {
-    gpio(port)->gpio_bshr = (1 << pin);       // Set the bit corresponding to the pin to 1, which sets the pin high
+    gpio(port)->gpio_bshr = (1 << pin);                            // Set the bit corresponding to the pin to 1, which sets the pin high
 }
 
-
-void gpio_clear_pin(GPIO_Port port, uint8_t pin)   // Function to clear the specified GPIO pin
+/*
+ * @brief : This function will clear the GPIOx PINx to RESET, 
+ * by setting "@BCR" register of that corresponding pin bit as 1.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ * 
+ */
+void gpio_clear_pin(GPIO_Port port, uint8_t pin)                   // Function to clear the specified GPIO pin
 {
     gpio(port)->gpio_bcr = (1 << pin);
 }
 
-void gpio_toggle_pin(GPIO_Port port, uint8_t pin)   // Function to toggle the specified GPIO pin
+/*
+ * @brief: This function will toggle the LED on that GPIOx and PINx, 
+ * by "@BSHR" corresponding bit to `XOR` with '1'. 
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ * 
+ */
+void gpio_toggle_pin(GPIO_Port port, uint8_t pin)                  // Function to toggle the specified GPIO pin
 {
     gpio(port)->gpio_bshr ^= (1 << pin);
 }
 
-uint8_t gpio_read_pin(GPIO_Port port, uint8_t pin)   // Function to read the state of the specified GPIO pin
+/*
+ * @brief: This function will read the pin status for digital input of '1',
+ * used to check the LED status if its `ON` or `OFF`.
+ * also, by reading the "@INDR" register corresponding Pin bit. 
+ * and checking by '&' with 1 so that we can get the proper reading. 
+ * @param :  GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : returning 'HIGH' and 'LOW' ['1' , '0'] value.
+ * 
+ */
+uint8_t gpio_read_pin(GPIO_Port port, uint8_t pin)                 // Function to read the state of the specified GPIO pin
 {
-    return (gpio(port)->gpio_indr >> pin) & 0x01;                   // Return the state of the pin (0 or 1)
+    return (gpio(port)->gpio_indr >> pin) & 0x01;                  // Return the state of the pin (0 or 1)
 }
--- task5/documentation/API_REFERENCE.md
+++ task5/documentation/API_REFERENCE.md
@@ -4,19 +4,92 @@
 
 ## GPIO DRIVER API : 
 
-```
+```c
 /* 
- * @brief this function initializes GPIO Port and Pins for the requirement of input or output mode.
+ * @brief this function initializes GPIO Port and Pins for the requirement of input or output mode. 
+ * Enabling the clock for required PORTx. "@RCC_APB2PCENR_REG" to turn on corresponding bit for PORTx.
  * @param port : [port A, port C , port D] ; pin : [0-7] pin ; in_out_mode : [1 (output) , 0 (input)].
  * @return nothing to return.
  */
 void gpio_init(GPIO_Port port, uint8_t pin, uint8_t in_out_mode);
-
 ```
 
+```c
+/*
+ * @brief : this function resets all the GPIO ports , so that its registers were fully cleaned before any application usage.
+ * by using "@RCC_APB2PRSTR_REG" for the corresponding bit for PORTx. by resetting it to clear position.
+ * @param : void there is no need for argument for this function as it resets all GPIO ports.
+ * @return : nothing to return.
+ *
+ */
+void gpio_reset_all(void);
 ```
 
+```c
+/*
+ * @brief : This function configuring GPIOx PINx as OUTPUT Mode. By configuring MODE and CNF using "@CFGLR" register, and make the pin as output Pull-up mode. and clear that pin before the writting on it by clearing "@BCR" register corresponding pin bit.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
+void gpio_config_output(GPIO_Port port, GPIO_PIN pin);
 ```
+
+```c
+/*
+ * @brief : this function configuring GPIOx PINx as INPUT mode. By configuring MODE and CNF using "@CFGLR" register, and make the pin as input-pull up in "@OUTDR" register ,so that that pin acts as input mode.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
+void gpio_config_input(GPIO_Port port,GPIO_PIN pin);
+```
+
+```c
+/*
+ * @brief : this function will set the GPIOx PINx to HIGH by setting the "@BSHR" registers corresponding bit to '1'.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ *
+ */
+void gpio_set_pin(GPIO_Port port, uint8_t pin);
+```
+
+```c
+/*
+ * @brief : This function will clear the GPIOx PINx to RESET, 
+ * by setting "@BCR" register of that corresponding pin bit as 1.
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ * 
+ */
+void gpio_clear_pin(GPIO_Port port, uint8_t pin);
+```
+
+```c
+/*
+ * @brief: This function will toggle the LED on that GPIOx and PINx, 
+ * by "@BSHR" corresponding bit to `XOR` with '1'. 
+ * @param : GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : nothing to return.
+ * 
+ */
+void gpio_toggle_pin(GPIO_Port port, uint8_t pin); 
+```
+
+```c
+/*
+ * @brief: This function will read the pin status for digital input of '1',
+ * used to check the LED status if its `ON` or `OFF`.
+ * also, by reading the "@INDR" register corresponding Pin bit. 
+ * and checking by '&' with 1 so that we can get the proper reading. 
+ * @param :  GPIO port : [GPIO A, GPIO C, GPIO D] , GPIO pin : [0 -7] pins.
+ * @return : returning 'HIGH' and 'LOW' ['1' , '0'] value.
+ * 
+ */
+uint8_t gpio_read_pin(GPIO_Port port, uint8_t pin)
+```
+
 
 ---
 
